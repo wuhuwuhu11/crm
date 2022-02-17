0def50bbe169aef4b88d2ea8c7d375aeabd6269f
wuhu <1256258358@qq.com>
2022-02-17 23:46:47 +0800

    第二次修改

---
+++ crm-test/src/main/java/com/bjpowernode/crm/settings/web/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.bjpowernode.crm.settings.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/settings/user")
+public class UserController {
+
+    /**
+     * 跳转到登录页面
+     * @return
+     */
+    @RequestMapping("/toLogin.do")
+    public String tologin(){
+        //applicationContext-web.xml
+        //凭借视图解析器的前缀+返回值+后缀名称，找到对应的jsp页面
+        //前缀=/WEB-INF/jsp
+        //返回值=/login
+        //后缀=.jsp
+        return "/login";
+    }
+}
